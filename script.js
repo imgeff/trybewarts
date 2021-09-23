c5ad9dea05290f7d1fc949f8d483e53d2802befb
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-09-23 13:23:18 -0300

    Corrige erros de linter parte 2 do Requisito 20

--- script.js
+++ script.js
@@ -26,8 +26,8 @@ function acceptVerificate() {
 agreementCheckBox.addEventListener('click', acceptVerificate);
 
 // Contador de Caracteres disponíveis
-const textAreaComent = document.getElementById('textarea'); //Captura o elemento TextArea
-const counterCaractere = document.getElementById('counter'); //Captura a tag p com o número 500
+const textAreaComent = document.getElementById('textarea'); // Captura o elemento TextArea
+const counterCaractere = document.getElementById('counter'); // Captura a tag p com o número 500
 function caractere() { // Função que será chamada no evento de keyup
   const subtraction = 500 - textAreaComent.value.length; // Pega quantos caracteres existem no textarea através do .length e subtrai por 500 que é o número maximo de caracteres
   counterCaractere.innerText = subtraction; // atualiza a tag p com o resultado da subtração  entre 500 e o número total de caracteres digitados até o momento
