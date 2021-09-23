6e195b93cfc6fd8dc20aeabdd1aa74c338c0c306
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-09-23 13:14:53 -0300

    Requisito 20

--- script.js
+++ script.js
@@ -24,3 +24,12 @@ function acceptVerificate() {
   }
 }
 agreementCheckBox.addEventListener('click', acceptVerificate);
+
+// Contador de Caracteres disponíveis
+const textAreaComent = document.getElementById('textarea');//Captura o elemento TextArea
+const counterCaractere = document.getElementById('counter');//Captura a tag p com o número 500
+function caractere() { // Função que será chamada no evento de keyup
+  const subtraction = 500 - textAreaComent.value.length; // Pega quantos caracteres existem no textarea através do .length e subtrai por 500 que é o número maximo de caracteres
+  counterCaractere.innerText = subtraction; // atualiza a tag p com o resultado da subtração  entre 500 e o número total de caracteres digitados até o momento
+}
+textAreaComent.addEventListener('keyup', caractere); // Invoca a função acima a cada tecla pressionada
